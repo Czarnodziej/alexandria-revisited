bc1f2341802414fe61ae50b69881f490613ed7fc
Czarnodziej <pagodemc@gmail.com>
2015-05-02 19:23:54 +0200

    making script more awesome

--- symlinks.sh
+++ symlinks.sh
@@ -9,13 +9,35 @@
 
 dir=~/dotfiles                    # dotfiles directory
 files="vimrc zshrc"               # list of files/folders to symlink in homedir
-
+username="czarnodziej"            # name of my personal config files
 ##########
+
+#### Remove old files
+
+if [-f ~/.vimrc]; then
+rm -rf ~/.vimrc
+fi
+
+if [! -f ~/.zshrc]; then
+rm -rf ~/.zshrc
+fi
+
+if [ -f ~/.oh-my-zsh/custom/plugins/$username/$username.plugin.zsh ]; then
+rm -rf ~/.oh-my-zsh/custom/plugins/$username/$username.plugin.zsh
+fi
+
+if [ -f ~/.oh-my-zsh/custom/themes/$username.zsh-theme ]; then
+rm -rf ~/.oh-my-zsh/custom/themes/$username.zsh-theme
+fi
+
+####
 
 for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+####
 
 install_vim_plug () {
 if [! -f ~/.vim/autoload/plug.vim]; then
@@ -24,7 +46,6 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 fi
 }
 
-
 install_zsh () {
 # Test to see if zshell is installed.  If it is:
 if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
@@ -32,9 +53,9 @@ if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
     if [[ ! -d $dir/oh-my-zsh/ ]]; then
     curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
     #symlink plugin with personal aliases...
-    mkdir ~/.oh-my-zsh/custom/plugins/czarnodziej; ln -s $dir/czarnodziej.plugin.zsh ~/.oh-my-zsh/custom/plugins/czarnodziej/czarnodziej.plugin.zsh
+    mkdir ~/.oh-my-zsh/custom/plugins/$username; ln -s $dir/$username.plugin.zsh ~/.oh-my-zsh/custom/plugins/$username/$username.plugin.zsh
     #...and bash prompt theme
-    mkdir ~/.oh-my-zsh/custom/themes; ln -s $dir/czarnodziej.zsh-theme ~/.oh-my-zsh/custom/themes/czarnodziej.zsh-theme
+    mkdir ~/.oh-my-zsh/custom/themes; ln -s $dir/$username.zsh-theme ~/.oh-my-zsh/custom/themes/$username.zsh-theme
     fi
     # Set the default shell to zsh if it isn't currently set to zsh
     if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
